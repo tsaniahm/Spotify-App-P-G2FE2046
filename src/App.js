01e4da478ceadf2d8df15da88c31ec5b8711223f
Tsaniah Munfidah <www.tsaniahmunfidah@gmail.com>
2022-03-23 11:09:47 +0700

    add components to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './styles/style.css';
-import { useEffect } from 'react';
+import Album from './components/albums';
+import data from './albumData/dataDummy';
+import CreatePlaylistForm from './components/createPlaylistForm';
 
 const SPOTIFY_CLIENT_ID = process.env;
 
@@ -13,41 +15,8 @@ function App() {
 
   return (
     <React.Fragment>
-      <div className="playlist-container">
-        <h1>CREATE PLAYLIST</h1>
-        <img src="images/spotify.png" alt="#" />
-        <form>
-          <label className="form-label" htmlFor="playlist-title">Playlist Title*</label>
-          <br />
-          <input
-            type="text"
-            className="input-title"
-            id="playlist-title"
-            defaultValue=""
-            placeholder="Input your playlist title"
-          />
-          <br />
-          <label className="form-label" htmlFor="playlist-desc">Description*</label>
-          <br />
-          <textarea
-            type="text"
-            className="input-desc"
-            id="playlist-desc"
-            defaultValue=""
-            placeholder="Input the description"
-          ></textarea>
-          <input id="submit" className="submit-button" type="button" defaultValue="CREATE" />
-        </form>
-      </div>
-      <div className="song-container">
-        <img src="images/album.jpg" alt="#" />
-        <h3 className="title">Death with Dignity</h3>
-        <p className="artist">Sufjan Stevens</p>
-        <p className="album">
-          Album . Carrie & Lowell . 2021
-        </p>
-        <button className="button-play">Add to playlist</button>
-      </div>
+      <CreatePlaylistForm />
+      <Album value={data}/>
     </React.Fragment>
   );
 }
